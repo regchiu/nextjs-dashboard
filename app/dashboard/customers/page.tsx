99b909b27688c092e26ce15b005006421470c9d6
regchiu <regx1385@gmail.com>
2024-01-08 11:07:02 +0800

    complete customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,38 @@
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+          Customers
+        </h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+    </div>
+  );
 }
